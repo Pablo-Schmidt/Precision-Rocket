b828662aee95472bbef34b0e49851a9d911c40d6
Pablo Schmidt <pablogschmidt2002@yahoo.com.ar>
2024-11-22 16:25:16 +0100

    Collision handler
    Collision Handler detects different tags with Switch

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision other)
     {
-        
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This is friendly");
+                break;
+            case "Fuel":
+                Debug.Log("This is fuel");
+                break;
+            case "Finish":
+                Debug.Log("This is the Finish pad!");
+                break;
+            default:
+                Debug.Log("Sorry you blew up");
+                break;
+       }
     }
 
 }
