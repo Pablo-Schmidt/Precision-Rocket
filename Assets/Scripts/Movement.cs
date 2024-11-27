3d8ad4c7122d31cab2c42ee2fc9a117195733977
Pablo Schmidt <pablogschmidt2002@yahoo.com.ar>
2024-11-27 16:13:28 +0100

    Background
    Background

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,9 +7,11 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100000f;
     [SerializeField] float rotationThrust = 50.0f;
     [SerializeField] AudioClip mainEngine;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
     [SerializeField] ParticleSystem leftThrustParticles;
     [SerializeField] ParticleSystem rightThrustParticles;
-    [SerializeField] ParticleSystem mainEngineParticles;
+
 
     Rigidbody rb;
     AudioSource audioSource;
@@ -32,44 +34,71 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(mainEngine);
-                
-            }
-            if (!mainEngineParticles.isPlaying)
-            {
-                mainEngineParticles.Play();
-            }
+            StartThrusting();
         }
         else
         {
-            audioSource.Stop();
-            mainEngineParticles.Stop();
+            StopThrusting();
         }
 
     }
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+
+        }
+        if (!mainEngineParticles.isPlaying)
+        {
+            mainEngineParticles.Play();
+        }
+    }
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+    }
+
     void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            leftThrustParticles.Play();
-            ApplyRotation(rotationThrust);
+            RotateLeft();
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
-            rightThrustParticles.Play();
-            ApplyRotation(-rotationThrust);
+            RotateRight();
         }
         else
         {
-            leftThrustParticles.Stop();
-            rightThrustParticles.Stop();
+            StopRotating();
         }
     }
+    void RotateLeft()
+    {
+        ApplyRotation(rotationThrust);
+        if (!leftThrustParticles.isPlaying)
+        {
+            leftThrustParticles.Play();
+        }
+    }
+    void RotateRight()
+    {
+        ApplyRotation(-rotationThrust);
+        if (!rightThrustParticles.isPlaying)
+        {
+            rightThrustParticles.Play();
+        }
+    }
+    void StopRotating()
+    {
+        leftThrustParticles.Stop();
+        rightThrustParticles.Stop();
+    }
 
-    private void ApplyRotation(float rotationThisFrame)
+    void ApplyRotation(float rotationThisFrame)
     {
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
     }
